78c923de50b4bc5c1fe641bb6cdd5857d2fd50f6
Fatima Arame Sarr <fatima9arame@gmail.com>
2026-02-26 15:38:33 +0000

    base de donnee et migrations

---
+++ database/migrations/2026_02_26_152242_create_cours_etudiant_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('cours_etudiant', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('etudiant_id')->constrained()->cascadeOnDelete();
+
+        $table->foreignId('cours_id')->constrained()->cascadeOnDelete();
+
+        $table->timestamps();
+    });
+}
+};
