ae39a220066fcd9219cc097f05c2339c15df8288
Fatima Arame Sarr <fatima9arame@gmail.com>
2026-03-23 16:29:32 +0000

    crud etudiants

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cours;
 
 class Etudiant extends Model
 {
+    protected $fillable = [
+        'prenom',
+        'nom',
+        'email',
+        'date_naissance'
+    ];
     public function cours()
 {
     return $this->belongsToMany(Cours::class);
--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EtudiantController;
 
-Route::prefix('v1/auth')->group(function () {
+Route::prefix('v1')->group(function () {
 
-    Route::post('/register', [AuthController::class, 'register']);
-
-    Route::post('/login', [AuthController::class, 'login']);
-
-    Route::middleware('auth:sanctum')->group(function () {
-
-        Route::post('/logout', [AuthController::class, 'logout']);
-
-        Route::get('/me', [AuthController::class, 'me']);
-
+    // AUTH PUBLIC
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
     });
 
+    // ROUTES PROTÉGÉES
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::prefix('auth')->group(function () {
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
+
+        // ✅ AJOUT ICI
+        Route::apiResource('etudiants', EtudiantController::class);
+
+    });
+    
 });
